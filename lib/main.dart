73a1ed2c488c6983a8513f95fd7775bcd47413f9
Eduardo <eduardo.gcandido@outlook.com>
2025-07-22 15:01:16 -0300

    adição de dados locais, sqflite e shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculator/extensions/imc_extension.dart';
 import 'package:imc_calculator/models/imc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,9 +34,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
-  TextEditingController pesoController = TextEditingController();
-  TextEditingController alturaController = TextEditingController();
+  late TextEditingController nomeController;
+  late TextEditingController pesoController;
+  late TextEditingController alturaController;
+  late SharedPreferences prefs;
+  ImcProvider _imcProvider = ImcProvider();
   List<Imc> imcs = [];
+
+  Future<String> initImcs() async{
+    _imcProvider.open();
+    prefs = await SharedPreferences.getInstance();
+    final String? nome = prefs.getString('nome');
+    final String? altura = prefs.getString('altura');
+    nomeController = TextEditingController(text: nome ?? '');
+    pesoController = TextEditingController();
+    alturaController = TextEditingController(text: altura ?? '');
+    await _imcProvider.getImcs().then((value) => imcs = value);
+
+    return nome ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,149 +66,186 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-                padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-                child: Text('Digite seu peso e sua altura para calcular seu IMC')
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    ),
-                    labelText: 'Peso',
+      body: FutureBuilder(
+        future: initImcs(),
+        builder: (context, asyncSnapshot) {
+          if(asyncSnapshot.hasData){
+            return Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                      padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                      child: Text('Digite seu peso e sua altura para calcular seu IMC')
                   ),
-                  keyboardType: TextInputType.number,
-                  controller: pesoController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor, insira seu peso';
-                    }
-                    return null;
-                  },
-                )
-            ),
-            Container(
-                padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    ),
-                    labelText: 'Altura',
+                  Container(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          labelText: 'Nome',
+                        ),
+                        keyboardType: TextInputType.text,
+                        controller: nomeController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira seu nome';
+                          }
+                          return null;
+                        },
+                      )
                   ),
-                  keyboardType: TextInputType.number,
-                  controller: alturaController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor, insira sua altura';
-                    }
-                    return null;
-                  },
-                )
-            ),
-            Container(
-                padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          labelText: 'Peso',
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: pesoController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira seu peso';
+                          }
+                          return null;
+                        },
+                      )
                   ),
-                  onPressed: (){
-                    if(_formKey.currentState!.validate()){
-                      Imc atualImc = Imc(
-                          peso: double.parse(pesoController.text),
-                          altura: double.parse(alturaController.text));
-                      imcs.add(atualImc);
+                  Container(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          labelText: 'Altura',
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: alturaController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Por favor, insira sua altura';
+                          }
+                          return null;
+                        },
+                      )
+                  ),
+                  Container(
+                      padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                          ),
+                          onPressed: () async{
+                            if(_formKey.currentState!.validate()){
+                              await calcularImcESalvar();
+                              setState(() {
+                                pesoController.clear();
+                              });
 
-                      setState(() {
-                        pesoController.clear();
-                        alturaController.clear();
-                      });
-
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('IMC calculado com sucesso!'),
-                        )
-                      );
-                    }
-                  },
-                  child: Text('Calcular'.toUpperCase())
-                )
-            ),
-            Flexible(
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: ListView.builder(
-                  itemCount: imcs.length,
-                  shrinkWrap: false,
-                  controller: ScrollController(),
-                  physics: ScrollPhysics(),
-                  itemBuilder: (context, index){
-                    final imc = imcs[index];
-                    return Column(
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              flex: 2,
-                              child: Column(
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('IMC calculado com sucesso!'),
+                                  )
+                              );
+                            }
+                          },
+                          child: Text('Calcular'.toUpperCase())
+                      )
+                  ),
+                  Flexible(
+                    child: Container(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ListView.builder(
+                        itemCount: imcs.length,
+                        shrinkWrap: false,
+                        controller: ScrollController(),
+                        physics: ScrollPhysics(),
+                        itemBuilder: (context, index){
+                          final imc = imcs[index];
+                          return Column(
+                            children: [
+                              Row(
                                 children: [
-                                  Card(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text('Peso: ${imc.peso}'),
+                                  Expanded(
+                                    flex: 2,
+                                    child: Column(
+                                      children: [
+                                        Card(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text('Peso: ${imc.peso}'),
+                                          ),
+                                        ),
+                                        Card(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text('Altura: ${imc.altura}'),
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  Card(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text('Altura: ${imc.altura}'),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              flex: 5,
-                              child: Column(
-                                children: [
-                                  Card(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text('IMC: ${imc.getImcValue().toStringAsFixed(1)}'),
-                                    ),
-                                  ),
-                                  Card(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text('Classificação: ${imc.getClassificacao()}'),
+                                  Expanded(
+                                    flex: 5,
+                                    child: Column(
+                                      children: [
+                                        Card(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text('IMC: ${imc.getImcValue().toStringAsFixed(1)}'),
+                                          ),
+                                        ),
+                                        Card(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text('Classificação: ${imc.getClassificacao()}'),
+                                          ),
+                                        )
+                                      ],
                                     ),
                                   )
                                 ],
                               ),
-                            )
-                          ],
-                        ),
-                        Divider()
-                      ],
-                    );
-                  },
-                ),
+                              Divider()
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
+            );
+          }else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
       ),
     );
+  }
+
+  Future<void> calcularImcESalvar() async{
+    await prefs.setString('altura', alturaController.text);
+    await prefs.setString('nome', nomeController.text);
+    Imc atualImc = Imc(
+        peso: double.parse(pesoController.text),
+        altura: double.parse(alturaController.text));
+
+    await _imcProvider.insert(atualImc);
+
+    imcs.add(atualImc);
   }
 }
 
--- lib/models/imc.dart
+++ lib/models/imc.dart
@@ -1,6 +1,92 @@
+import 'package:sqflite/sqflite.dart';
+
 class Imc {
+  int? id;
   double peso;
   double altura;
 
-  Imc({required this.peso, required this.altura});
+  Imc({this.id, required this.peso, required this.altura});
+
+  Map<String, Object?> toMap() {
+    var map = <String, Object?>{
+      'Id': id,
+      'Peso': peso,
+      'Altura': altura,
+    };
+    return map;
+  }
+
+  factory Imc.fromMap(Map<dynamic, dynamic> map) {
+    return Imc(
+      id: map['Id'],
+      peso: double.parse(map['Peso'].toString()),
+      altura: double.parse(map['Altura'].toString()),
+    );
+  }
+}
+
+class ImcProvider {
+  final String tableName = 'IMC';
+  final String columnId = 'Id';
+  final String columnAltura = 'Altura';
+  final String columnPeso = 'Peso';
+  final String pathDb = 'imc.db';
+
+  Future open() async {
+    var db = await openDatabase(pathDb, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute('''
+create table $tableName ( 
+  $columnId integer primary key autoincrement, 
+  $columnAltura numeric not null,
+  $columnPeso numeric not null)
+''');
+        });
+  }
+
+  Future<Imc> insert(Imc imc) async {
+    var db = await openDatabase(pathDb);
+    imc.id = await db.insert(tableName, imc.toMap());
+    return imc;
+  }
+
+  Future<List<Imc>> getImcs() async {
+    var db = await openDatabase(pathDb);
+    List<Map> maps = await db.query(tableName,
+        columns: [columnId, columnPeso, columnAltura]);
+
+    List<Imc> resultList = [];
+
+    for(var item in maps){
+      resultList.add(Imc.fromMap(item));
+    }
+
+    return resultList;
+  }
+
+  Future<Imc> getImc(int id) async {
+    var db = await openDatabase(pathDb);
+    List<Map> maps = await db.query(tableName,
+        columns: [columnId, columnPeso, columnAltura],
+        where: '$columnId = ?',
+        whereArgs: [id]);
+
+      return Imc.fromMap(maps.first);
+  }
+
+  Future<int> delete(int id) async {
+    var db = await openDatabase(pathDb);
+    return await db.delete(tableName, where: '$columnId = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(Imc todo) async {
+    var db = await openDatabase(pathDb);
+    return await db.update(tableName, todo.toMap(),
+        where: '$columnId = ?', whereArgs: [todo.id]);
+  }
+
+  Future close() async {
+    var db = await openDatabase(pathDb);
+    db.close();
+  }
 }
